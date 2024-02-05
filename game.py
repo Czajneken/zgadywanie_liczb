2d85ebefc2de98ac1e27599db28ac130cca2578a
Maciej Czaja <czaja.maciej.8@gmail.com>
2024-02-05 19:55:24 +0100

    Gotowa gra

---
+++ game.py
@@ -0,0 +1,35 @@
+import random
+
+def get_number():
+    """
+    Get a number from the user.
+
+    Try until the user will input proper number.
+
+    :rtype: int
+    :return: given number as int
+    """
+    while True:
+        try:
+            result = int(input("Guess the number: "))
+            break
+        except ValueError:
+            print("It's not a number!")
+
+    return result
+
+def game():
+    """The main function of the game."""
+    random_number = random.randint(1, 2)
+    user_number = get_number()
+
+    while user_number != random_number:
+        if user_number < random_number:
+            print("Too small!")
+        else:
+            print("Too big!")
+        user_number = get_number()
+    print("You win!")
+
+if __name__ == "__main__":
+    game()
